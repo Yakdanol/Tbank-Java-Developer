40aafd8a412b0384d7f6e4dce702b06718849dea
Yakdanol <danilaolegovich@yandex.ru>
2024-10-10 16:12:15 +0300

    Подключил Caffeine Cache для хранения данных

--- Homework/src/main/java/org/yakdanol/homework/config/AppConfig.java
+++ Homework/src/main/java/org/yakdanol/homework/config/AppConfig.java
@@ -1,11 +1,14 @@
 package org.yakdanol.homework.config;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Configuration
@@ -14,7 +17,10 @@ public class AppConfig {
 
     @Bean
     public CacheManager hourlyCache() {
-        log.info("Initializing hourly cache manager");
-        return new ConcurrentMapCacheManager("currencyRates");
+        log.info("Initializing hourly cache manager with Caffeine");
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager("currencyRates");
+        cacheManager.setCaffeine(Caffeine.newBuilder()
+                .expireAfterWrite(1, TimeUnit.HOURS)); // Данные будут истекать через 1 час
+        return cacheManager;
     }
 }
