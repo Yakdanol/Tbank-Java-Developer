5e5c0b2de45f110e08e5b9407da5d47689ccb2e1
Yakdanol <danilaolegovich@yandex.ru>
2024-10-10 14:43:23 +0300

    Добавил ожидаемые исключения и общий handler для них

---
+++ Homework/src/main/java/org/yakdanol/homework/exception/BadRequestException.java
@@ -0,0 +1,7 @@
+package org.yakdanol.homework.exception;
+
+public class BadRequestException extends RuntimeException {
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
---
+++ Homework/src/main/java/org/yakdanol/homework/exception/CustomExceptionHandler.java
@@ -0,0 +1,34 @@
+package org.yakdanol.homework.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.yakdanol.homework.model.ApiErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestControllerAdvice
+public class CustomExceptionHandler {
+
+    @ExceptionHandler(CurrencyNotFoundException.class)
+    public ResponseEntity<ApiErrorResponse> handleCurrencyNotFound(CurrencyNotFoundException e) {
+        log.error("Currency not found: {}", e.getMessage());
+        ApiErrorResponse response = new ApiErrorResponse(404, e.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ApiErrorResponse> handleBadRequest(BadRequestException e) {
+        log.warn("Bad request: {}", e.getMessage());
+        ApiErrorResponse response = new ApiErrorResponse(400, e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ApiErrorResponse> handleGeneralException(RuntimeException e) {
+        log.error("Internal server error: {}", e.getMessage());
+        ApiErrorResponse response = new ApiErrorResponse(500, "Internal server error");
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+    }
+}
