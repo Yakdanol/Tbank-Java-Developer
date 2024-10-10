af60afc4935103df4bb8b50bdd66615741cab16f
Yakdanol <danilaolegovich@yandex.ru>
2024-10-10 14:31:53 +0300

    Добавил модель данных ApiErrorResponse для последующей валидации ошибок. Small fix

--- Homework/src/main/java/org/yakdanol/homework/model/ConversionRequest.java
+++ Homework/src/main/java/org/yakdanol/homework/model/ConversionRequest.java
@@ -1,8 +1,10 @@
 package org.yakdanol.homework.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class ConversionRequest {
 
     private String fromCurrency;
