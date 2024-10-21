e909bd0e31986bcdfde754db2e99b309f0a8c526
Yakdanol <danilaolegovich@yandex.ru>
2024-10-21 14:28:17 +0300

    Добавил модуль для task5_6

---
+++ Homework/task5_6/src/main/java/org/yakdanol/task5_6/Task56Application.java
@@ -0,0 +1,13 @@
+package org.yakdanol.task5_6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task56Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Task56Application.class, args);
+    }
+
+}
