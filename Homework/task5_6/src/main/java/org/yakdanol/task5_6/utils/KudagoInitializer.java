17b108327fc8cafb4fdb5da7cf5ecc79a31b4351
Yakdanol <danilaolegovich@yandex.ru>
2024-10-22 21:09:36 +0300

    Изменил класс KudagoInitializer для асинхронного запуска инициализации данных

--- Homework/task5_6/src/main/java/org/yakdanol/task5_6/utils/KudagoInitializer.java
+++ Homework/task5_6/src/main/java/org/yakdanol/task5_6/utils/KudagoInitializer.java
@@ -1,39 +1,54 @@
 package org.yakdanol.task5_6.utils;
 
-import org.springframework.web.client.RestTemplate;
-import org.yakdanol.task5_6.annotation.LogExecutionTime;
-import org.yakdanol.task5_6.model.Category;
-import org.yakdanol.task5_6.model.Location;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+
 import org.yakdanol.task5_6.repository.CategoryRepository;
 import org.yakdanol.task5_6.repository.LocationRepository;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
-import javax.annotation.PostConstruct;
+import org.yakdanol.task5_6.model.Category;
+import org.yakdanol.task5_6.model.Location;
+import org.springframework.web.client.RestTemplate;
 
-@Component
 @Slf4j
+@Component
+@ComponentScan(basePackages = {"org.yakdanol.task8"})
 public class KudagoInitializer {
 
-    private static final String CATEGORIES_API_URL = "https://kudago.com/public-api/v1/place-categories/";
+    private static final String CATEGORIES_API_URL = "https://kudago.com/public-api/v1.4/place-categories/";
     private static final String LOCATIONS_API_URL = "https://kudago.com/public-api/v1.4/locations/";
 
     private final CategoryRepository categoryRepository;
     private final LocationRepository locationRepository;
     private final RestTemplate restTemplate;
+    private final ExecutorService fixedThreadPool;
 
-    public KudagoInitializer(CategoryRepository categoryRepository, LocationRepository locationRepository) {
+    public KudagoInitializer(CategoryRepository categoryRepository, LocationRepository locationRepository,
+                             @Qualifier("fixedThreadPool") ExecutorService fixedThreadPool) {
         this.categoryRepository = categoryRepository;
         this.locationRepository = locationRepository;
+        this.fixedThreadPool = fixedThreadPool;
         this.restTemplate = new RestTemplate();
     }
 
     @PostConstruct
-    @LogExecutionTime
     public void init() {
-        log.info("Starting data initialization from KudaGo API...");
+        log.info("Starting parallel initialization of data from KudaGo API...");
 
+        CompletableFuture<Void> categoryFuture = CompletableFuture.runAsync(this::initializeCategories, fixedThreadPool);
+        CompletableFuture<Void> locationFuture = CompletableFuture.runAsync(this::initializeLocations, fixedThreadPool);
+        CompletableFuture.allOf(categoryFuture, locationFuture).join();
+
+        log.info("Data initialization completed successfully.");
+    }
+
+    private void initializeCategories() {
         try {
-            // Инициализация категорий
             Category[] categories = restTemplate.getForObject(CATEGORIES_API_URL, Category[].class);
             if (categories != null) {
                 for (Category category : categories) {
@@ -43,8 +58,13 @@ public class KudagoInitializer {
             } else {
                 log.warn("No categories retrieved from KudaGo API.");
             }
+        } catch (Exception e) {
+            log.error("Error during category initialization: ", e);
+        }
+    }
 
-            // Инициализация городов
+    private void initializeLocations() {
+        try {
             Location[] locations = restTemplate.getForObject(LOCATIONS_API_URL, Location[].class);
             if (locations != null) {
                 for (Location location : locations) {
@@ -54,11 +74,8 @@ public class KudagoInitializer {
             } else {
                 log.warn("No locations retrieved from KudaGo API.");
             }
-
         } catch (Exception e) {
-            log.error("Error during initialization from KudaGo API: ", e);
+            log.error("Error during location initialization: ", e);
         }
-
-        log.info("Data initialization completed.");
     }
 }
