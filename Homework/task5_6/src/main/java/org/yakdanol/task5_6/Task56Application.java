f8cab984ccfec577c95148fdb596351ddd77bae8
Yakdanol <danilaolegovich@yandex.ru>
2024-10-28 20:38:47 +0300

    Добавил EventSpecification

--- Homework/task5_6/src/main/java/org/yakdanol/task5_6/Task56Application.java
+++ Homework/task5_6/src/main/java/org/yakdanol/task5_6/Task56Application.java
@@ -2,8 +2,10 @@ package org.yakdanol.task5_6;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "org.yakdanol.task5_6.model.repository")
 public class Task56Application {
 
 	public static void main(String[] args) {
