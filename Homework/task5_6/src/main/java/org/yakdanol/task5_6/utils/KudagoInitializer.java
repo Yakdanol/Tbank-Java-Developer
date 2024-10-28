6b339ed60741800e8a8aaf3e26dd5a1445129551
Yakdanol <danilaolegovich@yandex.ru>
2024-10-28 21:31:57 +0300

    Изменил инициализацию категорией и локаций в классе KudagoInitializer

--- Homework/task5_6/src/main/java/org/yakdanol/task5_6/utils/KudagoInitializer.java
+++ Homework/task5_6/src/main/java/org/yakdanol/task5_6/utils/KudagoInitializer.java
@@ -1,20 +1,25 @@
 package org.yakdanol.task5_6.utils;
 
-import org.springframework.web.client.RestTemplate;
-import org.yakdanol.task5_6.annotation.LogExecutionTime;
-import org.yakdanol.task5_6.model.Category;
-import org.yakdanol.task5_6.model.Location;
-import org.yakdanol.task5_6.repository.CategoryRepository;
-import org.yakdanol.task5_6.repository.LocationRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+import org.yakdanol.task5_6.annotation.LogExecutionTime;
+import org.yakdanol.task5_6.dto.CategoryDTO;
+import org.yakdanol.task5_6.dto.LocationDTO;
+import org.yakdanol.task5_6.model.entity.Category;
+import org.yakdanol.task5_6.model.entity.Location;
+import org.yakdanol.task5_6.model.repository.CategoryRepository;
+import org.yakdanol.task5_6.model.repository.LocationRepository;
+
 import javax.annotation.PostConstruct;
+import java.util.Arrays;
 
 @Component
 @Slf4j
 public class KudagoInitializer {
 
-    private static final String CATEGORIES_API_URL = "https://kudago.com/public-api/v1/place-categories/";
+    private static final String CATEGORIES_API_URL = "https://kudago.com/public-api/v1.4/place-categories/";
     private static final String LOCATIONS_API_URL = "https://kudago.com/public-api/v1.4/locations/";
 
     private final CategoryRepository categoryRepository;
@@ -34,31 +39,67 @@ public class KudagoInitializer {
 
         try {
             // Инициализация категорий
-            Category[] categories = restTemplate.getForObject(CATEGORIES_API_URL, Category[].class);
-            if (categories != null) {
-                for (Category category : categories) {
-                    categoryRepository.save(category.getId(), category);
-                }
-                log.info("Categories initialized successfully with {} entries.", categories.length);
-            } else {
-                log.warn("No categories retrieved from KudaGo API.");
-            }
+            initializeCategories();
 
-            // Инициализация городов
-            Location[] locations = restTemplate.getForObject(LOCATIONS_API_URL, Location[].class);
-            if (locations != null) {
-                for (Location location : locations) {
-                    locationRepository.save(location.getSlug(), location);
-                }
-                log.info("Locations initialized successfully with {} entries.", locations.length);
-            } else {
-                log.warn("No locations retrieved from KudaGo API.");
-            }
+            // Инициализация локаций
+            initializeLocations();
 
         } catch (Exception e) {
             log.error("Error during initialization from KudaGo API: ", e);
         }
 
         log.info("Data initialization completed.");
+    }
+
+    private void initializeCategories() {
+        log.info("Fetching categories from KudaGo API...");
+        try {
+            CategoryDTO[] categoriesDTO = restTemplate.getForObject(CATEGORIES_API_URL, CategoryDTO[].class);
+
+            if (categoriesDTO != null && categoriesDTO.length > 0) {
+                Arrays.stream(categoriesDTO).forEach(this::saveCategory);
+                log.info("Categories initialized successfully with {} entries.", categoriesDTO.length);
+            } else {
+                log.warn("No categories retrieved from KudaGo API.");
+            }
+        } catch (HttpClientErrorException e) {
+            log.error("Error fetching categories from KudaGo API: {}", e.getMessage());
+        }
+    }
+
+    private void saveCategory(CategoryDTO categoryDTO) {
+        Category category = new Category();
+        category.setSlug(categoryDTO.getSlug());
+        category.setName(categoryDTO.getName());
+
+        // Сохранение категории в репозитории
+        categoryRepository.save(category);
+        log.info("Category saved: {}", category);
+    }
+
+    private void initializeLocations() {
+        log.info("Fetching locations from KudaGo API...");
+        try {
+            LocationDTO[] locationsDTO = restTemplate.getForObject(LOCATIONS_API_URL, LocationDTO[].class);
+
+            if (locationsDTO != null && locationsDTO.length > 0) {
+                Arrays.stream(locationsDTO).forEach(this::saveLocation);
+                log.info("Locations initialized successfully with {} entries.", locationsDTO.length);
+            } else {
+                log.warn("No locations retrieved from KudaGo API.");
+            }
+        } catch (HttpClientErrorException e) {
+            log.error("Error fetching locations from KudaGo API: {}", e.getMessage());
+        }
+    }
+
+    private void saveLocation(LocationDTO locationDTO) {
+        Location location = new Location();
+        location.setSlug(locationDTO.getSlug());
+        location.setName(locationDTO.getName());
+
+        // Сохранение локации в репозитории
+        locationRepository.save(location);
+        log.info("Location saved: {}", location);
     }
 }
