2f349ee802d29b110a0818fc3f3a9cc46a4c74de
Yakdanol <danilaolegovich@yandex.ru>
2024-10-28 19:45:25 +0300

    Создал кастомную ошибку EventNotFoundException и отредактировал GlobalExceptionHandler

--- Homework/task5_6/src/main/java/org/yakdanol/task5_6/exception/GlobalExceptionHandler.java
+++ Homework/task5_6/src/main/java/org/yakdanol/task5_6/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package org.yakdanol.task5_6.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -18,8 +19,23 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 
+    @ExceptionHandler(EventNotFoundException.class)
+    public ResponseEntity<String> handleEventNotFound(EventNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
     @ExceptionHandler(EmptyRepositoryException.class)
     public ResponseEntity<String> handleEmptyRepository(EmptyRepositoryException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NO_CONTENT);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        return new ResponseEntity<>("Invalid input: " + ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGeneralException(Exception ex) {
+        return new ResponseEntity<>("An error occurred: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
