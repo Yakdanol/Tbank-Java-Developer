50e62afa4d9f7a8fad2da2b36cda74df81c5e078
Yakdanol <danilaolegovich@yandex.ru>
2024-10-28 19:37:46 +0300

    Переписал контроллеры LocationController и CategoryController, создал EventController

--- Homework/task5_6/src/main/java/org/yakdanol/task5_6/controller/CategoryController.java
+++ Homework/task5_6/src/main/java/org/yakdanol/task5_6/controller/CategoryController.java
@@ -1,45 +1,53 @@
 package org.yakdanol.task5_6.controller;
 
-import org.yakdanol.task5_6.annotation.LogExecutionTime;
-import org.yakdanol.task5_6.model.Category;
+import lombok.extern.slf4j.Slf4j;
+import org.yakdanol.task5_6.dto.CategoryDTO;
 import org.yakdanol.task5_6.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.concurrent.ConcurrentMap;
+import jakarta.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/places/categories")
-@LogExecutionTime
+@Slf4j
 public class CategoryController {
 
     private final CategoryService categoryService;
 
+    @Autowired
     public CategoryController(CategoryService categoryService) {
         this.categoryService = categoryService;
     }
 
-    @GetMapping
-    public ConcurrentMap<Long, Category> getAllCategories() {
-        return categoryService.getAllCategories();
+    @PostMapping
+    public ResponseEntity<CategoryDTO> createCategory(@RequestBody @Valid CategoryDTO categoryDTO) {
+        log.info("Received request to create category: {}", categoryDTO);
+        CategoryDTO createdCategory = categoryService.createCategory(categoryDTO);
+        return new ResponseEntity<>(createdCategory, HttpStatus.CREATED);
     }
 
     @GetMapping("/{id}")
-    public Category getCategoryById(@PathVariable Long id) {
-        return categoryService.getCategoryById(id);
+    public ResponseEntity<CategoryDTO> getCategoryById(@PathVariable Long id) {
+        log.info("Received request to get category by ID: {}", id);
+        CategoryDTO categoryDTO = categoryService.findCategoryById(id);
+        return new ResponseEntity<>(categoryDTO, HttpStatus.OK);
     }
 
-    @PostMapping
-    public Category createCategory(@RequestBody Category category) {
-        return categoryService.createCategory(category.getId(), category);
-    }
-
-    @PutMapping("/{id}")
-    public Category updateCategory(@PathVariable Long id, @RequestBody Category category) {
-        return categoryService.updateCategory(id, category);
+    @GetMapping
+    public ResponseEntity<List<CategoryDTO>> getAllCategories() {
+        log.info("Received request to get all categories");
+        List<CategoryDTO> categories = categoryService.findAllCategories();
+        return new ResponseEntity<>(categories, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteCategory(@PathVariable Long id) {
-        categoryService.deleteCategory(id);
+    public ResponseEntity<Void> deleteCategoryById(@PathVariable Long id) {
+        log.info("Received request to delete category by ID: {}", id);
+        categoryService.deleteCategoryById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
--- Homework/task5_6/src/main/java/org/yakdanol/task5_6/controller/LocationController.java
+++ Homework/task5_6/src/main/java/org/yakdanol/task5_6/controller/LocationController.java
@@ -1,45 +1,53 @@
 package org.yakdanol.task5_6.controller;
 
-import org.yakdanol.task5_6.annotation.LogExecutionTime;
-import org.yakdanol.task5_6.model.Location;
+import lombok.extern.slf4j.Slf4j;
+import org.yakdanol.task5_6.dto.LocationDTO;
 import org.yakdanol.task5_6.service.LocationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.concurrent.ConcurrentMap;
+import jakarta.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/locations")
-@LogExecutionTime
+@Slf4j
 public class LocationController {
 
     private final LocationService locationService;
 
+    @Autowired
     public LocationController(LocationService locationService) {
         this.locationService = locationService;
     }
 
-    @GetMapping
-    public ConcurrentMap<String, Location> getAllLocations() {
-        return locationService.getAllLocations();
-    }
-
-    @GetMapping("/{slug}")
-    public Location getLocationBySlug(@PathVariable String slug) {
-        return locationService.getLocationBySlug(slug);
-    }
-
     @PostMapping
-    public Location createLocation(@RequestBody Location location) {
-        return locationService.createLocation(location.getSlug(), location);
+    public ResponseEntity<LocationDTO> createLocation(@RequestBody @Valid LocationDTO locationDTO) {
+        log.info("Received request to create location: {}", locationDTO);
+        LocationDTO createdLocation = locationService.createLocation(locationDTO);
+        return new ResponseEntity<>(createdLocation, HttpStatus.CREATED);
     }
 
-    @PutMapping("/{slug}")
-    public Location updateLocation(@PathVariable String slug, @RequestBody Location location) {
-        return locationService.updateLocation(slug, location);
+    @GetMapping("/{id}")
+    public ResponseEntity<LocationDTO> getLocationById(@PathVariable Long id) {
+        log.info("Received request to get location by ID: {}", id);
+        LocationDTO locationDTO = locationService.findLocationById(id);
+        return new ResponseEntity<>(locationDTO, HttpStatus.OK);
     }
 
-    @DeleteMapping("/{slug}")
-    public void deleteLocation(@PathVariable String slug) {
-        locationService.deleteLocation(slug);
+    @GetMapping
+    public ResponseEntity<List<LocationDTO>> getAllLocations() {
+        log.info("Received request to get all locations");
+        List<LocationDTO> locations = locationService.findAllLocations();
+        return new ResponseEntity<>(locations, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteLocationById(@PathVariable Long id) {
+        log.info("Received request to delete location by ID: {}", id);
+        locationService.deleteLocationById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
