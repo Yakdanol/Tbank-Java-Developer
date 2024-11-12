3a2246d8d1340ceac7faa6d560e28a1784a854ca
Yakdanol <danilaolegovich@yandex.ru>
2024-11-12 15:33:15 +0300

    Создал класс JwtAuthenticationFilter для обрабатывания HTTP-запросов и проверки JWT токена

---
+++ Homework/task8/src/main/java/org/yakdanol/task8/auth/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,41 @@
+package org.yakdanol.task8.auth.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.yakdanol.task8.auth.service.UserService;
+import org.yakdanol.task8.auth.util.JwtUtil;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+    private final UserService userService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String token = request.getHeader("Authorization");
+        if (token != null && jwtUtil.validateToken(token)) {
+            if (!jwtUtil.isTokenExpired(token)) { // Проверка, истек ли токен
+                String username = jwtUtil.extractUsername(token);
+                var userDetails = userService.loadUserByUsername(username);
+
+                UsernamePasswordAuthenticationToken authToken =
+                        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
