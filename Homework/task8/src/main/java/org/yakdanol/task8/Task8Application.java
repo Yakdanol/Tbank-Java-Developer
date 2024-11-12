ef34771c8a6cc070abb2537f47e1b2e6fac12847
Yakdanol <danilaolegovich@yandex.ru>
2024-11-12 15:40:28 +0300

    [Fix] Исправил проблему области видимости repository в сервисе функционала аутентификации

--- Homework/task8/src/main/java/org/yakdanol/task8/Task8Application.java
+++ Homework/task8/src/main/java/org/yakdanol/task8/Task8Application.java
@@ -3,9 +3,11 @@ package org.yakdanol.task8;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"org.yakdanol.task8", "org.yakdanol.task7", "org.yakdanol.task5_6"})
+@EnableJpaRepositories("org.yakdanol.task8.auth.repository")
 public class Task8Application {
 
 	public static void main(String[] args) {
